64de5e82b66e10b3682a3f06227608d1bc930d53
yasserelkhiraoui <elkhiraouiyasser2@gmail.com>
2024-04-01 17:28:29 +0200

    first commit

---
+++ src/main/java/ma/enset/jpaenset/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package ma.enset.jpaenset.repositories;
+
+import ma.enset.jpaenset.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface RoleRepository  extends JpaRepository<Role,Long> {
+    Role findByRoleName(String roleName);
+}
+
---
+++ src/main/java/ma/enset/jpaenset/service/UserService.java
@@ -0,0 +1,14 @@
+package ma.enset.jpaenset.service;
+
+import ma.enset.jpaenset.entities.Role;
+import ma.enset.jpaenset.entities.User;
+
+public interface UserService {
+    User addNewUser(User user);
+    Role addNewRole(Role role);
+    User findUserByUserName(String userName);
+    Role findRoleName(String roleName);
+    void addRoleToUser(String username ,String roleName);
+    User authenticate(String userName, String password);
+
+}
